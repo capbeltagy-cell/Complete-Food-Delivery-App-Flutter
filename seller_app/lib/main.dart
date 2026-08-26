06ded619b355d6025f093cab31f15421ca0c8247
capbeltagy-cell <capbeltagy@gmail.com>
2026-08-26 17:05:44 +0300

    fix: connect merchant app to Dierb Firebase project

--- seller_app/lib/main.dart
+++ seller_app/lib/main.dart
@@ -4,10 +4,18 @@ import 'package:seller_app/global/global.dart';
 import 'package:seller_app/splashScreen/splash_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+const _dierbFirebase = FirebaseOptions(
+  apiKey: 'AIzaSyBVUGFEPhyNrFwkMjEuV4PGk7EEQS_CQ5I',
+  appId: '1:365123606367:android:82969f06df11aba2b8c8ea',
+  messagingSenderId: '365123606367',
+  projectId: 'dierb-29548',
+  storageBucket: 'dierb-29548.firebasestorage.app',
+);
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   sharedPreferences = await SharedPreferences.getInstance();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: _dierbFirebase);
   runApp(const MyApp());
 }
 
@@ -22,6 +30,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF166534)),
         scaffoldBackgroundColor: const Color(0xFFF7F8F5),
+        inputDecorationTheme: const InputDecorationTheme(filled: true, fillColor: Colors.white),
       ),
       builder: (context, child) => Directionality(textDirection: TextDirection.rtl, child: child!),
       home: const MySplashScreen(),
