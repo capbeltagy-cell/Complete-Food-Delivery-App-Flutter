024d49bca9a39ee991f23828c6bb4c115a472e5b
capbeltagy-cell <capbeltagy@gmail.com>
2026-08-27 15:17:19 +0300

    fix: connect rider app to Dierb Firebase

--- rider_app/lib/main.dart
+++ rider_app/lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rider_app/splashScreen/splash_screen.dart';
-
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'global/global.dart';
 
+const _dierbFirebase = FirebaseOptions(
+  apiKey: 'AIzaSyBVUGFEPhyNrFwkMjEuV4PGk7EEQS_CQ5I',
+  appId: '1:365123606367:android:82969f06df11aba2b8c8ea',
+  messagingSenderId: '365123606367',
+  projectId: 'dierb-29548',
+  storageBucket: 'dierb-29548.firebasestorage.app',
+);
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   sharedPreferences = await SharedPreferences.getInstance();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: _dierbFirebase);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,8 +32,12 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF166534)),
         scaffoldBackgroundColor: const Color(0xFFF7F8F5),
+        inputDecorationTheme: const InputDecorationTheme(filled: true, fillColor: Colors.white),
       ),
-      builder: (context, child) => Directionality(textDirection: TextDirection.rtl, child: child!),
+      builder: (context, child) => Directionality(
+        textDirection: TextDirection.rtl,
+        child: child ?? const SizedBox.shrink(),
+      ),
       home: const MySplashScreen(),
     );
   }
